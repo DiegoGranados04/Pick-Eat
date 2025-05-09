85f572a581d09ee83a28d6a0ac4970676e68bfdf
Ulises-Candelario <ucandelario@ucol.mx>
2025-05-09 12:27:33 -0600

    Cambios finales de Cafeterias

--- public/scripts/tiendasExpandible.js
+++ public/scripts/tiendasExpandible.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const cafeterias = [
     {
+      id: "central",
       nombre: "Cafetería Central",
       ubicacion: "Campus Central",
       horario: "7:00am - 3:00pm",
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
       imagen: "/images/img-central.jpg"
     },
     {
+      id: "telematica",
       nombre: "Café Telemática",
       ubicacion: "Campus Central",
       horario: "8:00am - 2:30pm",
@@ -17,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
       imagen: "https://via.placeholder.com/400x250?text=Café+Telemática"
     },
     {
+      id: "snack",
       nombre: "Snack Plaza",
       ubicacion: "Campus Central",
       horario: "9:00am - 4:00pm",
